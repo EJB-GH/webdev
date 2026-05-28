7e20c0bee6f2b2d322ba78874c6de50847051648
EJB9-GH <evanjbuck9@gmail.com>
2026-05-28 13:19:08 -0700

    added .js features, and formatted and added form to welcome etc. Done without videos as they are not up yet

--- 09-nodejs/app.js
+++ 09-nodejs/app.js
@@ -12,11 +12,11 @@ const server = http.createServer((req, res) => {
     file.serveFile('/welcome.html', 200, {}, req, res);
   }
   // form route
-  else if (req.method === 'GET' && req.url === '') {
-    // fill out this route
+  else if (req.method === 'GET' && req.url === '/form') {
+    file.serveFile('/form.html', 200, {}, req, res);
   }
   // form submission
-  else if (req.method === 'POST' && req.url === '') {
+  else if (req.method === 'POST' && req.url === '/formExerciseSubmit') {
     let body = '';
 
     req.on('data', (chunk) => {
